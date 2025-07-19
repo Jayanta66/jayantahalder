ee3c13f594f88d6715ef1dfbd04511a9efbf2a17
Jayanta66 <jayhalder18@gmail.com>
2025-07-19 13:02:57 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from './components/Contact'
-import Freelancing from './components/Freelancing'
 import Aos from "aos";
 import "aos/dist/aos.css"
 
@@ -21,8 +20,6 @@ const App = () => {
       <Navbar />
       <div className="container">
         <Home />
-        <Freelancing/>
-        <br></br>
         <Experience />
         <Skills />
         <Projects />
