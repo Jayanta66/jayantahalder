d39822d8270b7e7753e096e149506bd7176c64bf
Jayanta66 <jayhalder18@gmail.com>
2025-07-19 14:40:22 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,6 +30,14 @@ const Home = () => {
       <div className="container home" id="home">
         <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
          <h1 ref={typedRef}></h1>
+                   <a
+            href={pdf}
+            download="Resume.pdf"
+            className="btn btn-outline-warning my-3"
+          >
+            Download Resume
+          </a>
+          
         </div>
         <div className="right">
           <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
